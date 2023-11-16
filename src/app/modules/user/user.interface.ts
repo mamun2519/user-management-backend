b842064f447760fd58ace56b0500ae448e32e44f
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-11-16 22:48:37 +0600

    add to user schema or model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,16 +1,16 @@
 import { Model } from "mongoose";
-
+// user interface
 export type IUser = {
-  id: string;
   first_name: string;
   last_name: string;
   gender: "Male" | "Female" | "Others";
   avatar: string;
+  email: string;
   domain: string;
   available: boolean;
 };
-export type User = Model<IUser, Record<string, unknown>>;
-
+export type UserModel = Model<IUser, Record<string, unknown>>;
+// user filters
 export type IUserFilters = {
   searchTerm: string;
   domain: string;
